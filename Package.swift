65ae009fb7ba443c97ecece847c64d39e48d19fe
Michael Redig <mredig@gmail.com>
2023-08-07 13:05:31 -0500

    (feat) added SIMD3 conveniences

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -18,9 +18,17 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+		.plugin(
+			name: "SIMDFixer",
+			capability: .command(
+				intent: .custom(verb: "simd-fixer", description: "Generate SIMD code"),
+				permissions: [.writeToPackageDirectory(reason: "Code Gen")])),
         .target(
             name: "VectorExtor",
-            dependencies: []),
+            dependencies: [],
+			plugins: [
+				"SIMDFixer"
+			]),
         .testTarget(
             name: "VectorExtorTests",
             dependencies: ["VectorExtor"]),
