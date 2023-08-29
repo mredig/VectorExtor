baccfbff94acb19760067c0e4ccb5e78050bdb83
Michael Redig <mredig@gmail.com>
2023-08-29 00:35:47 -0500

    (refactor) improved closed range extensions

--- Sources/VectorExtor/ClosedRange+Extensions.swift
+++ Sources/VectorExtor/ClosedRange+Extensions.swift
@@ -5,7 +5,7 @@ import CoreGraphics
 
 public extension ClosedRange where Bound: BinaryFloatingPoint {
 	/// In a range, the value of a given relative location between bounds. For example, in the range `20...40`, the point `0.5` would be `30`
-	func interpolated(at point: Double, clipped: Bool = true) -> Bound {
+	func interpolated(at point: Bound, clipped: Bool = true) -> Bound {
 		let point = clipped ? Swift.max(0, Swift.min(1, point)) : point
 
 		let normalUpper = upperBound - lowerBound
@@ -15,9 +15,9 @@ public extension ClosedRange where Bound: BinaryFloatingPoint {
 	}
 
 	/// In a range, the relative location of a value between bounds. For example, if a range were `20...40`, the value `30` would be `0.5`
-	func linearPoint(of value: Bound, clipped: Bool = true) -> Double {
-		let normalUpper = Double(upperBound) - Double(lowerBound)
-		let normalValue = Double(value) - Double(lowerBound)
+	func linearPoint(of value: Bound, clipped: Bool = true) -> Bound {
+		let normalUpper = Bound(upperBound) - Bound(lowerBound)
+		let normalValue = Bound(value) - Bound(lowerBound)
 		return clipped ? Swift.min(Swift.max(normalValue / normalUpper, 0), 1) : normalValue / normalUpper
 	}
 }
