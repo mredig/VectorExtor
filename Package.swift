e7e4b4bf2d417cbef6405c7d14112573dcc7a945
Michael Redig <mredig@gmail.com>
2024-11-14 16:43:44 -0600

    (fix) simd generator plugin

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.8
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -18,11 +18,11 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-//		.plugin(
-//			name: "SIMDGenerator",
-//			capability: .command(
-//				intent: .custom(verb: "simd-generator", description: "Generate SIMD code"),
-//				permissions: [.writeToPackageDirectory(reason: "Code Gen")])),
+		.plugin(
+			name: "SIMDGenerator",
+			capability: .command(
+				intent: .custom(verb: "simd-generator", description: "Generate SIMD code"),
+				permissions: [.writeToPackageDirectory(reason: "Code Gen")])),
         .target(
             name: "VectorExtor",
             dependencies: []),
