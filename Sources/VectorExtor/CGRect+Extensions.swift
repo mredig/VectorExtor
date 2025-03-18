12b28729649e8d089863c96ccdff05e064dee2cf
Michael Redig <mredig@gmail.com>
2025-03-17 21:01:25 -0500

    (feat) add CGRect centering (with tests)

--- Sources/VectorExtor/CGRect+Extensions.swift
+++ Sources/VectorExtor/CGRect+Extensions.swift
@@ -22,4 +22,43 @@ public extension CGRect {
 	init(size: CGSize) {
 		self.init(origin: .zero, size: size)
 	}
+
+	/// Takes the current CGRect and offsets the origin such that the center of both `self` and `outerRect` are
+	/// overlaid on each other. This will also have the effect of making the left and right edges of each
+	/// CGRect equidistant to each other (inner left -> outer left == inner right -> outer right)
+	/// - Parameter outerRect: The CGRect that is stationary.
+	/// - Returns: Modified CGRect that is centered
+	@inline(__always)
+	func centered(inside outerRect: CGRect) -> CGRect {
+		let hBuffer = (outerRect.width - width) / 2
+		let vBuffer = (outerRect.height - height) / 2
+
+		return CGRect(origin: outerRect.origin + CGPoint(x: hBuffer, y: vBuffer), size: size)
+	}
+
+	/// Takes the current CGRect and offsets the origin such that the distance from the left edge of `self` and
+	/// `outerRect` are equal to the distance between the right edges.
+	/// - Parameter outerRect: The CGRect that is stationary.
+	/// - Returns: Modified CGRect that is centered horizontally
+	@inline(__always)
+	func centeredHorizontally(inside outerRect: CGRect) -> CGRect {
+		let hBuffer = (outerRect.width - width) / 2
+
+		return CGRect(
+			origin: CGPoint(x: outerRect.origin.x + hBuffer, y: origin.y),
+			size: size)
+	}
+
+	/// Takes the current CGRect and offsets the origin such that the distance from the left edge of `self` and
+	/// `outerRect` are equal to the distance between the right edges.
+	/// - Parameter outerRect: The CGRect that is stationary.
+	/// - Returns: Modified CGRect that is centered horizontally
+	@inline(__always)
+	func centeredVerticaly(inside outerRect: CGRect) -> CGRect {
+		let vBuffer = (outerRect.height - height) / 2
+
+		return CGRect(
+			origin: CGPoint(x: origin.x, y: outerRect.origin.y + vBuffer),
+			size: size)
+	}
 }
--- Tests/VectorExtorTests/CGRectExtensionsTests.swift
+++ Tests/VectorExtorTests/CGRectExtensionsTests.swift
@@ -1,44 +1,138 @@
-import XCTest
+import Testing
+import Foundation
 import VectorExtor
 
-class CGRectExtensionsTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-	func testRectUtilities() {
+struct CGRectExtensionsTests {
+	@Test func rectUtilities() {
 		let size = CGSize(width: 10, height: 50)
 		let rect = CGRect(origin: .zero, size: size)
-		XCTAssertEqual(rect.maxXY, size.point)
+		#expect(rect.maxXY == size.point)
 
-		XCTAssertEqual(rect.midPoint, CGPoint(x: 5, y: 25))
+		#expect(rect.midPoint == CGPoint(x: 5, y: 25))
 
 		let orig2 = CGPoint(x: 10, y: -10)
 		let rect2 = CGRect(origin: orig2, size: size)
-		XCTAssertEqual(rect2.maxXY, (size + orig2).point)
+		#expect(rect2.maxXY == (size + orig2).point)
 
-		XCTAssertEqual(rect2.midPoint, CGPoint(x: 15, y: 15))
+		#expect(rect2.midPoint == CGPoint(x: 15, y: 15))
 
-		XCTAssertEqual(rect.maxXMinY, CGPoint(x: 10, y: 0))
-		XCTAssertEqual(rect.minXMaxY, CGPoint(x: 0, y: 50))
+		#expect(rect.maxXMinY == CGPoint(x: 10, y: 0))
+		#expect(rect.minXMaxY == CGPoint(x: 0, y: 50))
 
-		XCTAssertEqual(rect2.maxXMinY, CGPoint(x: 20, y: -10))
-		XCTAssertEqual(rect2.minXMaxY, CGPoint(x: 10, y: 40))
+		#expect(rect2.maxXMinY == CGPoint(x: 20, y: -10))
+		#expect(rect2.minXMaxY == CGPoint(x: 10, y: 40))
 	}
 
-	func testSizeInit() {
+	@Test func sizeInit() {
 		let size = CGSize(width: 10, height: 50)
 
-		XCTAssertEqual(CGRect(x: 0, y: 0, width: 10, height: 50), CGRect(size: size))
+		#expect(CGRect(x: 0, y: 0, width: 10, height: 50) == CGRect(size: size))
 	}
 
-	func testScalar() {
+	@Test func scalar() {
 		let scalar = CGRect(scalarOrigin: 3.5, scalarSize: 4.5)
-		XCTAssertEqual(scalar, CGRect(x: 3.5, y: 3.5, width: 4.5, height: 4.5))
+		#expect(scalar == CGRect(x: 3.5, y: 3.5, width: 4.5, height: 4.5))
+	}
+
+	@Test(arguments: [
+		(
+			CGRect(scalarOrigin: 0, scalarSize: 20),
+			CGRect(scalarOrigin: 0, scalarSize: 10),
+			CGRect(origin: CGPoint(x: 5, y: 5), size: CGSize(scalar: 10))
+		),
+		(
+			CGRect(scalarOrigin: -100, scalarSize: 200),
+			CGRect(scalarOrigin: 0, scalarSize: 10),
+			CGRect(origin: CGPoint(x: -5, y: -5), size: CGSize(scalar: 10))
+		),
+		(
+			CGRect(origin: .zero, size: CGSize(width: 10, height: 20)),
+			CGRect(origin: CGPoint(x: 3_902_875, y: 878), size: CGSize(width: 4, height: 2)),
+			CGRect(origin: CGPoint(x: 3, y: 9), size: CGSize(width: 4, height: 2))
+		),
+		(
+			CGRect(origin: .zero, size: CGSize(width: 20, height: 10)),
+			CGRect(origin: CGPoint(x: -3_902_875, y: -878), size: CGSize(width: 4, height: 2)),
+			CGRect(origin: CGPoint(x: 8, y: 4), size: CGSize(width: 4, height: 2))
+		),
+
+		(
+			CGRect(origin: .zero, size: CGSize(width: 4, height: 2)),
+			CGRect(origin: CGPoint(x: -3_902_875, y: -878), size: CGSize(width: 10, height: 20)),
+			CGRect(origin: CGPoint(x: -3, y: -9), size: CGSize(width: 10, height: 20))
+		),
+	])
+	func centering(base: CGRect, toCenter: CGRect, expected: CGRect) {
+		let result = toCenter.centered(inside: base)
+
+		#expect(expected == result)
+	}
+
+	@Test(arguments: [
+		(
+			CGRect(scalarOrigin: 0, scalarSize: 20),
+			CGRect(scalarOrigin: 0, scalarSize: 10),
+			CGRect(origin: CGPoint(x: 5, y: 0), size: CGSize(scalar: 10))
+		),
+		(
+			CGRect(scalarOrigin: -100, scalarSize: 200),
+			CGRect(scalarOrigin: 0, scalarSize: 10),
+			CGRect(origin: CGPoint(x: -5, y: 0), size: CGSize(scalar: 10))
+		),
+		(
+			CGRect(origin: .zero, size: CGSize(width: 10, height: 20)),
+			CGRect(origin: CGPoint(x: 3_902_875, y: 878), size: CGSize(width: 4, height: 2)),
+			CGRect(origin: CGPoint(x: 3, y: 878), size: CGSize(width: 4, height: 2))
+		),
+		(
+			CGRect(origin: .zero, size: CGSize(width: 20, height: 10)),
+			CGRect(origin: CGPoint(x: -3_902_875, y: -878), size: CGSize(width: 4, height: 2)),
+			CGRect(origin: CGPoint(x: 8, y: -878), size: CGSize(width: 4, height: 2))
+		),
+
+		(
+			CGRect(origin: .zero, size: CGSize(width: 4, height: 2)),
+			CGRect(origin: CGPoint(x: -3_902_875, y: -878), size: CGSize(width: 10, height: 20)),
+			CGRect(origin: CGPoint(x: -3, y: -878), size: CGSize(width: 10, height: 20))
+		),
+	])
+	func centeringHorizontal(base: CGRect, toCenter: CGRect, expected: CGRect) {
+		let result = toCenter.centeredHorizontally(inside: base)
+
+		#expect(expected == result)
+	}
+
+	@Test(arguments: [
+		(
+			CGRect(scalarOrigin: 0, scalarSize: 20),
+			CGRect(scalarOrigin: 0, scalarSize: 10),
+			CGRect(origin: CGPoint(x: 0, y: 5), size: CGSize(scalar: 10))
+		),
+		(
+			CGRect(scalarOrigin: -100, scalarSize: 200),
+			CGRect(scalarOrigin: 0, scalarSize: 10),
+			CGRect(origin: CGPoint(x: 0, y: -5), size: CGSize(scalar: 10))
+		),
+		(
+			CGRect(origin: .zero, size: CGSize(width: 10, height: 20)),
+			CGRect(origin: CGPoint(x: 3_902_875, y: 878), size: CGSize(width: 4, height: 2)),
+			CGRect(origin: CGPoint(x: 3_902_875, y: 9), size: CGSize(width: 4, height: 2))
+		),
+		(
+			CGRect(origin: .zero, size: CGSize(width: 20, height: 10)),
+			CGRect(origin: CGPoint(x: -3_902_875, y: -878), size: CGSize(width: 4, height: 2)),
+			CGRect(origin: CGPoint(x: -3_902_875, y: 4), size: CGSize(width: 4, height: 2))
+		),
+
+		(
+			CGRect(origin: .zero, size: CGSize(width: 4, height: 2)),
+			CGRect(origin: CGPoint(x: -3_902_875, y: -878), size: CGSize(width: 10, height: 20)),
+			CGRect(origin: CGPoint(x: -3_902_875, y: -9), size: CGSize(width: 10, height: 20))
+		),
+	])
+	func centeringVertical(base: CGRect, toCenter: CGRect, expected: CGRect) {
+		let result = toCenter.centeredVerticaly(inside: base)
+
+		#expect(expected == result)
 	}
 }
